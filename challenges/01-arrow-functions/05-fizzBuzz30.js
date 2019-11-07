064ec737219f190ce63f5ee57b3196c2cfd8241f
Louis Sutton <louis.sutton.iii@gmail.com>
2019-11-06 16:12:43 -0800

    FizzBuzz30

--- challenges/01-arrow-functions/05-fizzBuzz30.js
+++ challenges/01-arrow-functions/05-fizzBuzz30.js
@@ -2,11 +2,19 @@
 //1 through 30 replacing numbers divisible by 3 with 'fizz', numbers divisible by 5 with 'buzz' and numbers divisible by 3 and 5 with
 //'fizzbuzz'
 
-let fizzBuzz30;
+const fizzBuzz30 = () => {
+let output = [];
+    for(i=1; i<= 30; i++) {
+        output.push(
+            i%15 === 0 ? 'fizzbuzz'
+           : i%3 === 0 ? 'fizz'
+           : i%5 === 0 ? 'buzz'
+           :i
 
-
-
-
+        )
+        }
+        return output
+    }
 
 
 
