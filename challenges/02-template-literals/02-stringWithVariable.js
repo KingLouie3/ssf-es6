a1295256206717403f77a5e37288498ebb8b93c7
Louis Sutton <louis.sutton.iii@gmail.com>
2019-11-06 16:49:04 -0800

    Passed

--- challenges/02-template-literals/02-stringWithVariable.js
+++ challenges/02-template-literals/02-stringWithVariable.js
@@ -1,9 +1,9 @@
 //Convert the string being returned by the below function to utilize template literal syntax.  Be sure to make use of string 
 //interpolation
 
-const stringWithVariable = _ => {
+const stringWithVariable = ()  => {
     const CITY = 'Defiance'; 
-    return 'Welcome to ' + CITY;
+    return `Welcome to ${CITY}`;
 };
 
 
