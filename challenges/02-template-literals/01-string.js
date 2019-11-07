7f6f334c2c05395364894a38a171765b11a5d072
Louis Sutton <louis.sutton.iii@gmail.com>
2019-11-06 16:42:05 -0800

    Passed

--- challenges/02-template-literals/01-string.js
+++ challenges/02-template-literals/01-string.js
@@ -1,10 +1,8 @@
 // Convert the below string to make use of teplate literals
 
-function string() {
-    return 'Marry had a little lamb';
-}
 
 
+const string = () => `Marry had a little lamb`;
 
 
 
